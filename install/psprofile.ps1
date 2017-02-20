38a9c0ceadcbd75877fc13ba0401df2e2dfbb9d7
Troy Pocklington <tdashroy@gmail.com>
2017-02-20 12:33:48 -0800

    Fixing powershell profile install to work regardless of where your git repo is located.

--- install/psprofile.ps1
+++ install/psprofile.ps1
@@ -1,2 +1,3 @@
-﻿". ~\git\dev\powershell\profile.ps1" | Out-File $profile.CurrentUserCurrentHost -Force
+﻿$dot = Split-Path $MyInvocation.MyCommand.Path
+". $dot\..\powershell\profile.ps1" | Out-File $profile.CurrentUserCurrentHost -Force
 . $profile.CurrentUserCurrentHost
