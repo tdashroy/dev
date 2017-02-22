cb2ba932640e4db25600e89af64523812d642a23
Troy Pocklington <tdashroy@gmail.com>
2017-02-21 23:30:33 -0800

    Using forward slashes instead of backward slashes for vimrc

--- install/vimprofile.ps1
+++ install/vimprofile.ps1
@@ -1,3 +1,4 @@
 ﻿$dot = Split-Path $MyInvocation.MyCommand.Path
 $vimrc = [System.IO.Path]::GetFullPath((Join-Path $dot "..\vim\_vimrc"))
+$vimrc = $vimrc -replace "\\", "/"
 "source $vimrc" | Out-File ~\_vimrc -Encoding ASCII -Force
