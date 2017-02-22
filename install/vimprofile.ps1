0bd96e1d6752b000d90901f6c887a53eaf6a6fea
Troy Pocklington <tdashroy@gmail.com>
2017-02-21 22:13:49 -0800

    Fixing vimprofile install to use ASCII encoding, vim doesn't seem to like UTF16

--- install/vimprofile.ps1
+++ install/vimprofile.ps1
@@ -1,3 +1,3 @@
 ﻿$dot = Split-Path $MyInvocation.MyCommand.Path
 $vimrc = [System.IO.Path]::GetFullPath((Join-Path $dot "..\vim\_vimrc"))
-"source $vimrc" | Out-File ~\_vimrc -Force
+"source $vimrc" | Out-File ~\_vimrc -Encoding ASCII -Force
