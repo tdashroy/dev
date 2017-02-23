4804490ec2b94a35676f6def80d9cf3d9acdf1d6
Troy Pocklington <trpockli@microsoft.com>
2017-02-22 19:02:21 -0800

    Changing vimprofile install to use execute for source, so that i can use fnameescape.

--- install/vimprofile.ps1
+++ install/vimprofile.ps1
@@ -1,4 +1,3 @@
 ﻿$dot = Split-Path $MyInvocation.MyCommand.Path
 $vimrc = [System.IO.Path]::GetFullPath((Join-Path $dot "..\vim\_vimrc"))
-$vimrc = $vimrc -replace "\\", "/"
-"source $vimrc" | Out-File ~\_vimrc -Encoding ASCII -Force
+"execute 'source ' . fnameescape('$vimrc')" | Out-File ~\_vimrc -Encoding ASCII -Force
