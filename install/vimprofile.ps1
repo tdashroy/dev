0514c670becdae33f77a3a14d4b5588f5a33c542
Troy Pocklington <tdashroy@gmail.com>
2017-03-01 23:00:39 -0800

    Adding _gvimrc to vimprofile install.

--- install/vimprofile.ps1
+++ install/vimprofile.ps1
@@ -1,3 +1,8 @@
-﻿$dot = Split-Path $MyInvocation.MyCommand.Path
-$vimrc = [System.IO.Path]::GetFullPath((Join-Path $dot "..\vim\_vimrc"))
+﻿$dotdir = Split-Path $MyInvocation.MyCommand.Path
+
+# Using ASCII because otherwise vim gets confused.
+$vimrc = [System.IO.Path]::GetFullPath((Join-Path $dotdir "..\vim\_vimrc"))
 "execute 'source ' . fnameescape('$vimrc')" | Out-File ~\_vimrc -Encoding ASCII -Force
+
+$gvimrc = [System.IO.Path]::GetFullPath((Join-Path $dotdir "..\vim\_gvimrc"))
+"execute 'source ' . fnameescape('$gvimrc')" | Out-File ~\_gvimrc -Encoding ASCII -Force
