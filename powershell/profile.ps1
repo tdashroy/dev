c1a952c934dfe51b6eaa24e7c725063208594678
Troy Pocklington <tdashroy@gmail.com>
2017-03-17 10:10:45 -0700

    Adding support for local powershell profile for machine specific settings.

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -2,3 +2,10 @@
 
 . $dot\posh-git.ps1
 . $dot\prompt.ps1
+
+# Local powershell profile, for machine specific settings
+$lprofile = "$dot\lprofile.ps1"
+if (Test-Path $lprofile)
+{
+    . $lprofile
+}
