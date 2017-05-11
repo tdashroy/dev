3166e5775bd0b42cf944ddf2e02cde0f78b3b31e
Troy Pocklington <tdashroy@gmail.com>
2017-05-10 22:47:34 -0700

    Using new directory structure.

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,11 @@
 ﻿$dot = Split-Path $MyInvocation.MyCommand.Path
 
-. $dot\posh-git.ps1
-. $dot\prompt.ps1
+. $dot\modules\posh-git.ps1
+. $dot\console\colors.ps1
+. $dot\console\prompt.ps1
+. $dot\functions\colors.ps1
+. $dot\functions\cd.ps1
+. $dot\aliases\cd.ps1
 
 # Local powershell profile, for machine specific settings
 $lprofile = "$dot\lprofile.ps1"
