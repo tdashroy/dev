237cfa080f1610039507b42874e62a3b9fe55060
Troy Pocklington <trpockli@microsoft.com>
2017-08-08 22:48:16 -0700

    Turning off bash-like tab completion by default.

--- powershell/console/prompt.ps1
+++ powershell/console/prompt.ps1
@@ -16,7 +16,7 @@
 }
 
 # Make powershell tab complete unix-like
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
+# Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 # Turn off beeps
 Set-PSReadlineOption -BellStyle None
