dc6694703c0567e34bbe00b45f2b8d5db076b9ad
Troy Pocklington <trpockli@microsoft.com>
2017-11-10 13:46:31 -0800

    No longer using vim, deleting config and install.

--- install/vimprofile.ps1
+++
@@ -1,8 +0,0 @@
-﻿$dotdir = Split-Path $MyInvocation.MyCommand.Path
-
-# Using ASCII because otherwise vim gets confused.
-$vimrc = [System.IO.Path]::GetFullPath((Join-Path $dotdir "..\vim\_vimrc"))
-"execute 'source ' . fnameescape('$vimrc')" | Out-File ~\_vimrc -Encoding ASCII -Force
-
-$gvimrc = [System.IO.Path]::GetFullPath((Join-Path $dotdir "..\vim\_gvimrc"))
-"execute 'source ' . fnameescape('$gvimrc')" | Out-File ~\_gvimrc -Encoding ASCII -Force
