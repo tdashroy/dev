508568b909936e208409d9dd80e78ba661453d84
Troy Pocklington <trpockli@microsoft.com>
2018-08-16 08:31:20 -0700

    Adding 'git remote prune origin' to gitBranchCleanup

--- powershell/functions/git.ps1
+++ powershell/functions/git.ps1
@@ -1,5 +1,6 @@
 function gitBranchCleanup {
     Write-Host "Cleaning up local git branches that no longer exist on the remote...";
+    git remote prune origin;
     $filtered = git branch -vv | Select-String -InputObject {$_} -Pattern "origin/.*: gone";
     if ($filtered)
     {
