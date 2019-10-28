e621a79aec067ae4435b1160efa0b94ed72fc328
Troy Pocklington <tdashroy@gmail.com>
2019-10-28 13:14:24 -0700

    Setting powershell oh-my-posh theme to paradox on install

--- windows/install/git.ps1
+++ windows/install/git.ps1
@@ -6,11 +6,17 @@
     return
 }
 
-PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowClobber
-PowerShellGet\Install-Module oh-my-posh -Scope CurrentUser -AllowClobber
-
 # Make line endings checkout Windows-style and commit Unix-style
 git config --global core.autocrlf true
 
 # Turn off .orig files after resolving conflicts with git mergetool
 git config --global mergetool.keepBackup false
+
+# Install posh-git for nicer powershell git integration
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowClobber
+
+# Install oh-my-posh for theming of the powershell prompt
+PowerShellGet\Install-Module oh-my-posh -Scope CurrentUser -AllowClobber
+
+# Set oh-my-posh theme to Paradox
+Set-Theme Paradox
