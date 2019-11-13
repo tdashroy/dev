5869ccf6c5aaf2aa849b12dcb4619876892d64b4
Troy Pocklington <tdashroy@gmail.com>
2019-11-12 23:57:38 -0800

    Re-enabling zsh install from general install script

--- linux/debian/install/install.sh
+++ linux/debian/install/install.sh
@@ -2,4 +2,4 @@
 git_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" >/dev/null 2>&1 && pwd )"
 source "$git_dir/linux/debian/install/git.sh"
 #source "$git_dir/linux/debian/install/vim.sh"
-#source "$git_dir/linux/debian/install/zsh.sh"
+source "$git_dir/linux/debian/install/zsh.sh"
