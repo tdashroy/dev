56db1fa82cd21142470b2e5cabf12a5fa9518e3b
Troy Pocklington <tdashroy@gmail.com>
2019-11-27 23:57:05 -0800

    wip on powershell version of generic run setup task function

--- windows/setup/common.psm1
+++ windows/setup/common.psm1
@@ -1,25 +1,123 @@
-Function Run-Install-Task {
-    Param(
-
-    )
-}
-
-Function Run-Uninstall-Task {
-    Param(
-
-    )
-}
 
 enum SetupType {
    Install
    Uninstall 
 }
 
+enum Ask {
+    Always
+    Never
+    Overwrite
+}
+
+enum UserInput {
+    All
+    New
+    None
+}
+
+Function Run-Install-Task {
+Param(
+    [Ask] $ask
+    [bool] $overwrite
+    [UserInput] $user_input
+    [bool] $input_required
+    [String] $install_string
+    [String] $overwrite_string
+    [ScriptBlock] $exists_cmd
+    [ScriptBlock] $install_cmd
+)
+    if ($input_required -eq $true -and $user_input -eq UserInput::None)
+    {
+        return 2
+    }
+
+    $task_string = $install_string
+    $exists = $false
+
+    & $exists_cmd
+    if ($lastexitcode -eq 0)
+    {
+        if ($overwrite -ne $true -or ($input_required -eq $true -and $input_required -ne UserInput::All))
+        {
+            return 2
+        }
+        $task_string = $overwrite_string
+        $exists = $true
+    }
+
+    if ($ask -eq Ask::Always -or ($exits -eq $true -and $ask -eq Ask::Overwrite))
+    {
+        <prompt> "Would you like to $($task_string)? [y/n] "
+    }
+
+    Write-Host "Running task to $($task_string)..."
+    & $install_cmd
+    if ($lastexitcode -ne 0)
+    {
+        Write-Host "Failed to $($task_string)."
+        return 1
+    }
+
+    return 0
+}
+
+Function Run-Uninstall-Task {
+Param(
+    [Ask] $ask
+    [String] $uninstall_string
+    [ScriptBlock] $exists_cmd
+    [ScriptBlock] $uninstall_cmd
+)
+    & $exists_cmd
+    if ($lastexitcode -ne 0)
+    {
+        Write-Host "Skipping task to $($uninstall_string)."
+        return 2
+    }
+
+    if ($ask -eq Ask::Always -or $ask -eq Ask::Overwrite)
+    {
+        <prompt> "Would you like to $($uninstall_string)? [y/n] "
+    }
+
+    Write-Host "Running task to $($uninstall_string)..."
+    & $uninstall_cmd
+    if ($lastexitcode -ne 0)
+    {
+        Write-Host "Failed to $($uninstall_string)."
+        return 1
+    }
+
+    return 0
+}
+
 Function Run-Setup-Task {
 Param(
-
+    [SetupType] $setup_type
+    [Ask] $ask
+    [bool] $overwrite
+    [UserInput] $user_input
+    [bool] $input_required
+    [String] $install_string
+    [String] $overwrite_string
+    [String] $uninstall_string
+    [ScriptBlock] $exists_cmd
+    [ScriptBlock] $install_cmd
+    [ScriptBlock] $uninstall_cmd
 )
-
+    if ($setup_type -eq SetupType::Install)
+    {
+        return Run-Install-Task $ask $overwrite $user_input $input_required $install_string $overwrite_string $exists_cmd $install_cmd
+    }
+    elseif ($setup_type -eq SetupType::Uninstall)
+    {
+        return Run-Uninstall-Task $ask $uninstall_string $exists_cmd $uninstall_cmd
+    }
+    else
+    {
+        return 1
+    }
 }
 
 Export-ModuleMember -Function * -Variable * -Alias *
--- windows/setup/git.ps1
+++ windows/setup/git.ps1
@@ -6,11 +6,23 @@
     return
 }
 
-# Make line endings checkout Windows-style and commit Unix-style
-# git config --global core.autocrlf true
-
 # standardize on LF for checkout and commit
-git config --global core.autocrlf input
+function git-autocrlf {
+    $autocrlf = git config --global --get core.autocrlf
+
+    $setup_type = $g_setup_type
+    $ask = $g_ask
+    $overwrite = $g_overwrite
+    $user_input = $g_user_input
+    $input_required = $false
+    $install_string = "set git autocrlf to input"
+    $overwrite_string = "overwrite git autocrlf from $autocrlf to input"
+    $uninstall_string = "unset git autocrlf from input"
+    function exists_cmd { $autocrlf -eq $null }
+    function install_cmd { git config --global core.autocrlf input }
+    function uninstall_cmd { git config --global --unset-all core.autocrlf input }
+    return Run-Setup-Task $setup_type $ask $overwrite $user_input $input_required $install_string $overwrite_string $uninstall_string { exists_cmd } { overwrite_cmd } { uninstall_cmd }
+}
 
 # Turn off .orig files after resolving conflicts with git mergetool
 git config --global mergetool.keepBackup false
