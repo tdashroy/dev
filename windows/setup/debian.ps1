05b6261826e72c435fe7cd3dfe501861ddea20a8
Troy Pocklington <tdashroy@gmail.com>
2019-12-16 01:50:59 -0800

    finishing windows wsl debian setup script

--- windows/setup/debian.ps1
+++ windows/setup/debian.ps1
@@ -8,13 +8,40 @@ Import-Module "$git_dir\windows\setup\common.psm1" -DisableNameChecking
 # parse common args
 . "$git_dir\windows\setup\args.ps1" @args
 
+# enable Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+function wsl-enable {
+    $feature_name = "Microsoft-Windows-Subsystem-Linux"
+   
+    $setup_type = $g_setup_type
+    $ask = $g_ask
+    $overwrite = $false
+    $user_input = $g_user_input
+    $input_required = $true
+    $install_string = "enable Windows Subsystem for Linux"
+    $overwrite_string = ""
+    $uninstall_string = "disable Windows Subsystem for Linux"
+    function exists_cmd { (Get-WindowsOptionalFeature -Online -FeatureName $feature_name | ForEach-Object State) -eq "Enabled" }
+    function install_cmd {
+        Write-Host "When prompted, please restart your system and re-run the setup script"
+        Enable-WindowsOptionalFeature -Online -FeatureName $feature_name
+        return $?
+    }
+    function uninstall_cmd { 
+        Disable-WindowsOptionalFeature -Online -FeatureName $feature_name -NoRestart
+        $ret = $?
+        Write-Host "Once uninstall script is finished running, please restart your machine for changes to take full effect"
+        return $ret
+    }
+    return Run-Setup-Task $setup_type $ask $overwrite $user_input $input_required $install_string $overwrite_string $uninstall_string { exists_cmd } { install_cmd } { uninstall_cmd }
+}
+
 # install git-for-windows
 function debian-install {    
     $setup_type = $g_setup_type
     $ask = $g_ask
     $overwrite = $false
     $user_input = $g_user_input
-    $input_required = $true
+    $input_required = $false
     $install_string = "install debian windows subsystem for linux"
     $overwrite_string = ""
     $uninstall_string = "uninstall debian windows subsystem for linux"
@@ -29,27 +56,78 @@ function debian-install {
         Add-AppxPackage $package
         $ret = $?; if (-not $ret) { return $ret }
         # initialize
-        Start-Process "debian.exe" -Wait
-        $ret = $?; return $ret
+        Start-Process -FilePath "debian.exe" -ArgumentList "install" -Wait -NoNewWindow
+        $ret = $?; if (-not $ret) { return $ret }
+        Start-Process -FilePath "debian.exe" -ArgumentList "-c sudo apt-get update && sudo apt-get -y upgrade" -Wait -NoNewWindow
     }
     function uninstall_cmd { 
         $package = Get-AppxPackage | Where-Object Name -Like "*debian*"
-        Remove-AppxPackage $package        
+        Remove-AppxPackage $package   
+        return $?     
+    }
+    return Run-Setup-Task $setup_type $ask $overwrite $user_input $input_required $install_string $overwrite_string $uninstall_string { exists_cmd } { install_cmd } { uninstall_cmd }
+}
+
+# run through debian setup tasks
+function debian-setup {
+    $setup_args = $args
+    Write-Host "setup_args: $setup_args"
+
+    $setup_type = $g_setup_type
+    $ask = $g_ask
+    $overwrite = $false
+    $user_input = $g_user_input
+    $input_required = $true
+    $install_string = "run through debian install tasks"
+    $overwrite_string = ""
+    $uninstall_string = "run through debian uninstall tasks"
+    function exists_cmd { 
+        # todo: maybe change this to something real? might actually be fine...tho it's pretty gross
+        return ((Get-Command "wsl" -ErrorAction SilentlyContinue) -and ($setup_type -eq "uninstall"))
+    }
+    function install_cmd {
+        $debian_setup_script = wsl wslpath -a "$git_dir\linux\debian\setup\setup.sh".Replace("\", "\\")
+        $debian_setup_cmd = "$debian_setup_script $setup_args"
+        Start-Process -FilePath "debian.exe" -ArgumentList "-c $debian_setup_cmd" -Wait -NoNewWindow
+        return $?
+    }
+    function uninstall_cmd {
+        $debian_setup_script = wsl wslpath -a "$git_dir\linux\debian\setup\setup.sh".Replace("\", "\\")
+        $debian_setup_cmd = "$debian_setup_script $setup_args"
+        Start-Process -FilePath "debian.exe" -ArgumentList "-c $debian_setup_cmd" -Wait -NoNewWindow
+        return $?        
     }
     return Run-Setup-Task $setup_type $ask $overwrite $user_input $input_required $install_string $overwrite_string $uninstall_string { exists_cmd } { install_cmd } { uninstall_cmd }
 }
 
 function install {
+    $ret = wsl-enable
+
+    switch($ret) {
+        0 { 
+            # don't want to continue if we had to enable wsl
+            exit 0 
+        }
+        1 { 
+            Write-Error "Skipping the rest of the debian setup"
+            return $ret 
+        }
+    }
+
     $ret = debian-install
+    $ret = debian-setup @args
     return $ret
 }
 
 function uninstall {
+    Write-Host "uninstall args: $args"
+    $ret = debian-setup @args
     $ret = debian-install
+    $ret = wsl-enable
     return $ret
 }
 
-$ret = & $g_setup_type
+$ret = & $g_setup_type @args
 
 # unload modules if this script loaded 
 if ($common_module -eq $null) { Remove-Module common }
