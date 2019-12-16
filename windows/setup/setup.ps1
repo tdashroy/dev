741a1265c0c0747e8b45130b2924cb6b4510beff
Troy Pocklington <tdashroy@gmail.com>
2019-12-16 01:54:57 -0800

    updating windows master setup script order

--- windows/setup/setup.ps1
+++ windows/setup/setup.ps1
@@ -1,15 +1,15 @@
 ﻿# elevate if not already
-f (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
     exit;
 }
 
 # check execution policy
-if ((Get-ExecutionPolicy).value__ -gt [Microsoft.PowerShell.ExecutionPolicy]::RemoteSigned.value__)
+if (((Get-ExecutionPolicy).value__ -ne [Microsoft.PowerShell.ExecutionPolicy]::Bypass.value__) -and ((Get-ExecutionPolicy).value__ -ne [Microsoft.PowerShell.ExecutionPolicy]::Unrestricted.value__))
 {
-    Write-Host "Please set the execution policy to allow RemoteSigned scripts to be run."
+    Write-Host "Please set the execution policy to Bypass scripts to be run."
     Write-Host "To achieve this, you can run the following command from an admin PowerShell:"
-    Write-Host "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Confirm"
+    Write-Host "Set-ExecutionPolicy Bypass -Scope CurrentUser -Confirm"
     return
 }
 
@@ -22,11 +22,11 @@ Import-Module "$git_dir\windows\setup\common.psm1" -DisableNameChecking
 . "$git_dir\windows\setup\args.ps1" @args
 
 & "$git_dir\windows\setup\git.ps1"
+& "$git_dir\windows\setup\debian.ps1" @args
 & "$git_dir\windows\setup\font.ps1"
-& "$git_dir\windows\setup\debian.ps1"
-& "$git_dir\windows\setup\terminal.ps1"
 & "$git_dir\windows\setup\powershell.ps1"
 & "$git_dir\windows\setup\powershell_core.ps1"
+& "$git_dir\windows\setup\terminal.ps1"
 
 # unload modules if this script loaded them
 if ($common_module -eq $null) { Remove-Module common }
