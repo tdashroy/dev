e77faec8e2a2bd862a048a0471b40b8e6e89be33
Troy Pocklington <tdashroy@gmail.com>
2020-01-18 01:10:27 -0800

    make zsh tab completion colors the same as ls

--- linux/debian/profile.zsh
+++ linux/debian/profile.zsh
@@ -3,3 +3,6 @@ git_dir="$( cd "$( dirname "${(%):-%x}" )/../../" >/dev/null 2>&1 && pwd )"
 
 # set up ls colors to look better with the OneHalfDark theme
 eval "$(dircolors "$git_dir/linux/debian/dircolors")"
+
+# make tab completion colors the same as dircolors
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
