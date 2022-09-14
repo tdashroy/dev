0e16a67dcc0af56228c4003c017c7118e78959a1
Troy Pocklington <tdashroy@gmail.com>
2022-09-14 13:40:56 -0700

    increasing zsh history size

--- linux/debian/profile.zsh
+++ linux/debian/profile.zsh
@@ -10,5 +10,9 @@ eval "$(dircolors "$git_dir/linux/debian/dircolors")"
 # make tab completion colors the same as dircolors
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
+# increase history size from omz defaults
+HISTSIZE=10000000
+SAVEHIST=5000000
+
 [ -d "$HOME/.local/bin" ] && export path=("$HOME/.local/bin" $path)
 [ -d "$HOME/bin" ] && export path=("$HOME/bin" $path)
